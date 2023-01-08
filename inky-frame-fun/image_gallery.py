9826bfb433aa6298080159bd18b66b8a78da7ad9
Chris Jenkins <cdpjenkins@gmail.com>
2023-01-08 20:11:29 +0000

    Clear screen before drawing image

--- inky-frame-fun/image_gallery.py
+++ inky-frame-fun/image_gallery.py
@@ -15,6 +15,12 @@ import uos
 
 import WIFI_CONFIG
 
+def enable_vsys_hold_to_prevent_sleep():
+    HOLD_VSYS_EN_PIN = 2
+
+    hold_vsys_en_pin = Pin(HOLD_VSYS_EN_PIN, Pin.OUT)
+    hold_vsys_en_pin.value(True)
+
 def setup_sd_card():
     sd_spi = SPI(0, sck=Pin(18, Pin.OUT), mosi=Pin(19, Pin.OUT), miso=Pin(16, Pin.OUT))
     sd = sdcard.SDCard(sd_spi, Pin(22))
@@ -28,8 +34,6 @@ def connect_network():
     network_manager = NetworkManager(WIFI_CONFIG.COUNTRY, status_handler=status_handler)
     uasyncio.get_event_loop().run_until_complete(network_manager.client(WIFI_CONFIG.SSID, WIFI_CONFIG.PSK))
     gc.collect()
-
-FILENAME = "/sd/placekitten.jpg"
 
 def load_text_from_url(url):
     connection = urequest.urlopen(url)
@@ -54,22 +58,9 @@ def download_image(filename):
     url = None
     gc.collect()
 
-display = PicoGraphics(display=DISPLAY_INKY_FRAME)
-
-# and the activity LED
-activity_led = Pin(6, Pin.OUT)
-
-# set up and enable vsys hold so we don't go to sleep
-HOLD_VSYS_EN_PIN = 2
-
-hold_vsys_en_pin = Pin(HOLD_VSYS_EN_PIN, Pin.OUT)
-hold_vsys_en_pin.value(True)
-
-# Create a new JPEG decoder for our PicoGraphics
-j = jpegdec.JPEG(display)
-
-print("Created jpegdec")
-print(gc.mem_free())
+def download_images():
+    for image_file in images_list:
+        download_image(image_file)
 
 def display_image(filename):
     path_on_sd = f"/sd/{filename}"
@@ -77,15 +68,13 @@ def display_image(filename):
     print(f"Opening {path_on_sd}")
     
     activity_led.on()
+    display.set_pen(1)
+    display.clear()
     j.open_file(path_on_sd)
     j.decode(0, 0, jpegdec.JPEG_SCALE_FULL)
     display.update()
     activity_led.off()
     gc.collect()
-
-def download_images():
-    for image_file in images_list:
-        download_image(image_file)
     
 def show_slideshow():
     while True:
@@ -93,14 +82,14 @@ def show_slideshow():
             display_image(image_file)
             time.sleep(60)
 
+
+display = PicoGraphics(display=DISPLAY_INKY_FRAME)
+j = jpegdec.JPEG(display)
+activity_led = Pin(6, Pin.OUT)
+
+enable_vsys_hold_to_prevent_sleep()
 setup_sd_card()
 connect_network()
-
-print("Set up SD card and network")
-print(gc.mem_free())
-
-# download_image()
-# display_image(FILENAME)
 
 images_list = load_text_from_url("{}/list.txt".format(WIFI_CONFIG.IMAGES_URL)).split("\n")
 print([image_file for image_file in images_list])
